ea07e5c9f277e1d1635f5ed0fd9edd4b387c749e
Arthur Menétrey <arthur.menetrey@icloud.com>
2025-12-11 14:07:18 +0100

    Added project

---
+++ LoadComboboxFilterIssue.Client/CommonServices.cs
@@ -0,0 +1,12 @@
+﻿using LoadComboboxFilterIssue.Services;
+
+namespace LoadComboboxFilterIssue.Client;
+
+public class CommonServices {
+    public static void Configure(IServiceCollection services, IConfiguration configuration) {
+        services.AddDevExpressBlazor(options => {
+            options.SizeMode = DevExpress.Blazor.SizeMode.Medium;
+        });
+        services.AddScoped<DxThemesService>();
+    }
+}
---
+++ LoadComboboxFilterIssue/Program.cs
@@ -0,0 +1,37 @@
+﻿using LoadComboboxFilterIssue.Client;
+using LoadComboboxFilterIssue.Components;
+using LoadComboboxFilterIssue.Client.Pages;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+CommonServices.Configure(builder.Services, builder.Configuration);
+builder.Services.AddMvc();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if(app.Environment.IsDevelopment()) {
+    app.UseWebAssemblyDebugging();
+} else {
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(LoadComboboxFilterIssue.Client.Pages.Index).Assembly)
+    .AllowAnonymous();
+
+app.Run();
